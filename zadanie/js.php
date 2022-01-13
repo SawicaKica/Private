a0b9824701ae4354a9016f18859e19025c7e5755
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2022-01-13 21:53:49 +0100

    ok

---
+++ zadanie/js.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Imie: ".$_GET['Imie']."<br>";
+	echo "Wiek: ".$_GET['Wiek']."<br>";
+	echo "Ukryte: ".$_GET['ukryte']."<br>";
+	echo "szkoła: ".$_GET['szkoła']."<br>";
+	echo "Ocena szkoły: ".$_GET['ocena']."<br>";
+	echo "Sprzeciwskazania: ".$_GET['rzeczy']."<br>";
+?>
